a83ddc3776dde1a9809fe1d44137d238ac85164b
yunusgnc <yunus0261@gmail.com>
2024-07-20 19:22:53 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function App() {
+const { width, height } = Dimensions.get('window');
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={['#0E2B14', '#2B9943', '#269251', '#E1FDA6', '#CCDEB4']}
+      style={styles.container}
+      locations={[0.05, 0.32, 0.53, 0.88, 1]}>
+      <View style={styles.rewardContainer}>
+        <Text style={styles.rewardText}>Günlük Ödül</Text>
+      </View>
+      <View style={styles.middleSection}>
+        <Text style={styles.gameText}>Strateji</Text>
+        <Text style={styles.gameText}>Eğlence</Text>
+      </View>
+      <View style={styles.createRoomContainer}>
+        <Text style={styles.createRoomText}>Siktik diye yorumladım</Text>
+      </View>
+    </LinearGradient>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width,
+    height,
+  },
+  rewardContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+  },
+  rewardText: {
+    color: 'white',
+    fontSize: 16,
+    marginTop: 10,
+  },
+  middleSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 50,
+  },
+  gameText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  createRoomContainer: {
+    alignItems: 'center',
+    marginTop: 30,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 10,
+    borderRadius: 10,
+  },
+  createRoomText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
+
+export default App;
